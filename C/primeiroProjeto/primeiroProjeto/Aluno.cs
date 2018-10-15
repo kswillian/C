690a9c7ff60cdfb5eff94a144696de42e3e06cf1
willian-santos <developer.williansantos@gmail.com>
2018-10-15 16:58:21 -0300

    Primeiro projeto POO

---
+++ C/primeiroProjeto/primeiroProjeto/Aluno.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace primeiroProjeto
+{
+    internal class Aluno
+    {
+        public string nome;
+        private float n1;
+        private float n2;
+        private float media;
+
+        public Aluno()
+        {
+
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void setN1(float n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void setN2(float n2)
+        {
+            this.n2 = n2;
+        }
+
+        public void setMedia(float media)
+        {
+            this.media = media;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public float getN1()
+        {
+            return this.n1;
+        }
+
+        public float getN2()
+        {
+            return this.n2;
+        }
+
+        public float getMedia()
+        {
+            return this.media;
+        }
+
+        public float calcularMedia(float n1, float n2)
+        {
+            this.media= (n1 + n2) / 2;
+
+            return this.media; 
+        }
+
+        public string calcularConceito(float media)
+        {
+            if (media > 7.0)
+            {
+                return "Aluno aprovado";
+
+            } else if (media > 5)
+            {
+
+                return "ALuno em recuparação";
+
+            }
+
+            return "Aluno reprovado";
+        }
+    }
+}
---
+++ C/primeiroProjeto/primeiroProjeto/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace primeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno a = new Aluno();
+
+            Console.Write("-|--------------------------|- \n" +
+                          " |       Dados do Aluno     | \n" +
+                          "-|--------------------------|-\n" +
+                          " | Nome: ");
+
+            a.setNome(Console.ReadLine());
+
+            Console.Write(" | Nota1: ");
+
+            a.setN1(float.Parse(Console.ReadLine()));
+
+            Console.Write(" | Nota2: ");
+
+            a.setN2(float.Parse(Console.ReadLine()));
+
+            Console.Write("\n" +
+                          "-|--------------------------|- \n" +
+                          " | Nome: {0} \n" +
+                          " | Nota1: {1} \n" +
+                          " | Nota2: {2} \n" +
+                          " | Média: {3} \n" +
+                          "-|--------------------------|- \n" +
+                          " | Conceito: {4} \n" +
+                          "-|--------------------------|-", 
+                          a.getNome(), a.getN1(), 
+                          a.getN2(), a.calcularMedia(a.getN1(), 
+                          a.getN2()), a.calcularConceito(a.getMedia()));
+
+            Console.ReadLine();
+
+        }
+    }
+}
